f04187fbfe75960ea788e1e1aef6a85c7a61d198
Rachit Bharadwaj <mail.rachit.bharadwaj@gmail.com>
2024-07-25 20:22:51 +0530

    chore: updated the balance section

--- components/dashboard/Balance.tsx
+++ components/dashboard/Balance.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Balance() {
   return (
-    <section className="bg-white p-5 rounded-xl flex-col flex gap-3">
+    <section className="bg-white border p-5 rounded-xl flex-col flex gap-3">
       <p>Balance</p>
 
       <div className="font-bold flex">
@@ -10,6 +10,34 @@ export default function Balance() {
         <p className="text-3xl">458</p>
       </div>
 
+      <div className="flex gap-3 flex-wrap lg:gap-5">
+        <div>
+          <p className="text-gray-400">Coupon</p>
+          <div className="font-semibold flex">
+            <p className="text-xs">$</p>
+            <p className="text-lg">458</p>
+          </div>
+        </div>
+
+        <div className="border" />
+
+        <div>
+          <p className="text-gray-400">Credits</p>
+          <div className="font-semibold flex">
+            <p className="text-xs">$</p>
+            <p className="text-lg">0</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex gap-3 flex-wrap">
+        <p className="text-gray-400">Hours Remaining</p>
+        <p>20:59:09</p>
+      </div>
+
+      <button className="border-primary-200 border text-primary-200 p-3 rounded-full">
+        Recharge Now
+      </button>
     </section>
   );
 }
