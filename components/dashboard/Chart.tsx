7a5d30296fd9c29ad1ec3c1480575104a0a80329
Rachit Bharadwaj <mail.rachit.bharadwaj@gmail.com>
2024-07-25 20:58:17 +0530

    feat: added actives jobs table and modified charts

--- components/dashboard/Chart.tsx
+++ components/dashboard/Chart.tsx
@@ -12,59 +12,31 @@ import {
 } from "recharts";
 
 const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
+  { date: "21 JAN", value: 100 },
+  { date: "22 JAN", value: 150 },
+  { date: "23 JAN", value: 180 },
+  { date: "24 JAN", value: 220 },
+  { date: "25 JAN", value: 160 },
 ];
 
-export default class Example extends PureComponent {
-  static demoUrl = "https://codesandbox.io/p/sandbox/simple-area-chart-4y9cnl";
+const CustomTooltip = ({ active, payload, label }: any) => {
+  if (active && payload && payload.length) {
+    return (
+      <div className="custom-tooltip bg-black text-white p-2 rounded">
+        <p className="label">{`${label} 2023`}</p>
+        <p className="intro">{`${payload[0].value}`}</p>
+      </div>
+    );
+  }
 
+  return null;
+};
+
+export default class Example extends PureComponent {
   render() {
     return (
-      <ResponsiveContainer width="100%" height="100%">
+      <ResponsiveContainer width="100%" height={400}>
         <AreaChart
-          width={500}
-          height={400}
           data={data}
           margin={{
             top: 10,
@@ -73,11 +45,23 @@ export default class Example extends PureComponent {
             bottom: 0,
           }}
         >
+          <defs>
+            <linearGradient id="colorValue" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+          </defs>
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
+          <XAxis dataKey="date" />
           <YAxis />
-          <Tooltip />
-          <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+          <Tooltip content={<CustomTooltip />} />
+          <Area
+            type="monotone"
+            dataKey="value"
+            stroke="#8884d8"
+            fillOpacity={1}
+            fill="url(#colorValue)"
+          />
         </AreaChart>
       </ResponsiveContainer>
     );
